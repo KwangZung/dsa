ff1769db87ae4e4a00e34b06668e5a35d40c009b
KwangZung <yungngq@gmail.com>
2024-09-26 10:17:53 +0700

    add Insert_a_node_at_the_head_of_a_linked_list.java

---
+++ Insert_a_node_at_the_head_of_a_linked_list.java
@@ -0,0 +1,6 @@
+ static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode head = new SinglyLinkedListNode(data);
+        head.next = llist;
+        return head;
+
+    }
